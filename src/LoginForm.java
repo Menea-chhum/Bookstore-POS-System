9ca405549419f4b868c9e1cc550de32edd151213
Menea-chhum <chhummenea17@gmail.com>
2026-07-13 13:56:24 +0700

    Initial commit

---
+++ src/LoginForm.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel mainPanel;
+    private JPanel LoginCard;
+    private JButton logInButton;
+    private JTextField usernameInput;
+    private JPasswordField passwordInput;
+    private JLabel storeLogo;
+    private JLabel failMsg;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/adminDashboard.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class adminDashboard {
+    private JPanel mainPanel;
+    private JLabel storeLogo;
+    private JLabel avatarLabel;
+    private JButton logoutBtn;
+    private JLabel staffNameLabel;
+    private JLabel staffrolePanel;
+    private JTabbedPane dashboardTab;
+    private JPanel booksPanel;
+    private JButton addBookBtn;
+    private JLabel totalTitleLabel;
+    private JLabel totalStockLabel;
+    private JLabel totalCategoryLabel;
+    private JLabel LowStockLable;
+    private JTextField bookSearchField;
+    private JComboBox categoryFilterCombo;
+    private JTable booksTable;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
