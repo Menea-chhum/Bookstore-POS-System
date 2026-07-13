96c8ef28851301018e98f249b6b9653502e217e4
Menea-chhum <chhummenea17@gmail.com>
2026-07-13 16:32:52 +0700

    work on category tab

---
+++ src/newSale.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class newSale {
+    private JPanel MainPanel;
+    private JTextField txtSeach;
+    private JButton btnSearch;
+    private JSpinner spinner1;
+    private JButton addToCartButton;
+    private JTable table1;
+    private JButton confirmButton;
+    private JButton cancelButton;
+    private JLabel Logo;
+    private JButton logOutButton;
+}
