01c4bf11de7f2e12ea60fff3dcd10fd9d0a81d5e
Menea-chhum <chhummenea17@gmail.com>
2026-07-15 11:14:31 +0700

    add

---
+++ src/addBookForm.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class addBookForm {
+    private JPanel mainPanel;
+    private JTextField enterBookTitleTextField;
+    private JTextField enterPriceTextField;
+    private JTextField enterAuthorTextField;
+    private JTextField enterStockQuantityTextField;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTable table1;
+    private JButton addToListButton;
+    private JButton a️SaveAllBooksButton;
+    private JButton clearAllButton;
+}
