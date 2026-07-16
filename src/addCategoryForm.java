48cefbfd921afa0691d0b71e904e9b596683b1b5
Menea-chhum <chhummenea17@gmail.com>
2026-07-16 15:40:36 +0700

    create supplier form

--- src/addCategoryForm.java
+++ src/addCategoryForm.java
@@ -2,10 +2,10 @@ import javax.swing.*;
 
 public class addCategoryForm {
     private JPanel mainPanel;
+    private JButton clearAllButton;
+    private JButton a️SaveAllCategoriesButton;
+    private JTable table1;
     private JTextField textField1;
     private JTextField textField2;
-    private JTable table1;
-    private JButton a️SaveAllCategoryButton;
-    private JButton clearAllButton;
     private JButton addToListButton;
 }
--- src/addSupplierForm.java
+++ src/addSupplierForm.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
 
-public class addBookForm {
+public class addSupplierForm {
     private JPanel mainPanel;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JButton a️SaveAllSuppliersButton;
+    private JButton clearAllButton;
+    private JTable table1;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JButton addToListButton;
-    private JButton a️SaveAllBookButton;
-    private JButton clearAllButton;
     private JLabel errMsg;
-    private JTable table1;
+    private JButton deleteButton;
 }
