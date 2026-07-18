db7630bc685583f9ba346b7c0dcfbb7e09114eef
tchou8319-tech <tchou8319@gmail.com>
2026-07-18 22:53:02 +0700

    Create saleRecord.java

---
+++ src/saleRecord.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class saleRecord {
+    private JPanel MainPanel;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField1;
+    private JTable table1;
+    private JTable table2;
+    private JButton searchButton;
+    private JTextField textField2;
+}
