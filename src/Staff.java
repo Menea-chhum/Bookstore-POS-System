c3d3aacdb28a98cc2e5fe97bd6aa2d9e610cc84c
Menea-chhum <chhummenea17@gmail.com>
2026-07-23 10:56:10 +0700

    add

---
+++ src/Staff.java
@@ -0,0 +1,20 @@
+public class Staff {
+    private  int  staffId;
+    private String username;
+    private String password;
+    private String full_name;
+    private String role;
+
+    public Staff(int staffId, String username, String password, String full_name, String role){
+        this.staffId = staffId;
+        this.username = username;
+        this.password = password;
+        this.full_name = full_name;
+        this.role = role;
+    }
+    public int getStaffId() { return staffId; }
+    public String getUsername() { return username; }
+    public String getPassword () { return password; }
+    public String getFull_name () { return full_name; }
+    public String getRole () { return role; }
+}
