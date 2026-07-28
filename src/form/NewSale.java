603b3d17f45bd6b2eba4e4663c6444236058d84f
tchou8319-tech <tchou8319@gmail.com>
2026-07-28 16:00:03 +0700

    Create NewSale.java

---
+++ src/form/NewSale.java
@@ -0,0 +1,12 @@
+package form;
+
+import javax.swing.*;
+
+public class NewSale {
+    private JPanel MainPanel;
+    private JPanel Header;
+    private JButton button1;
+    private JTextField textField1;
+    private JButton a︎Button;
+    private JTable table1;
+}
