673d0df9b43309ccb4f975f5a641d45069ed5f46
tchou8319-tech <tchou8319@gmail.com>
2026-07-28 18:03:38 +0700

    Update NewSale.java

--- src/form/NewSale.java
+++ src/form/NewSale.java
@@ -9,4 +9,9 @@ public class NewSale {
     private JTextField textField1;
     private JButton a︎Button;
     private JTable table1;
+    private JSpinner spinner1;
+    private JButton addToCartButton;
+    private JTable table2;
+    private JButton cancelButton;
+    private JButton confirmButton;
 }
