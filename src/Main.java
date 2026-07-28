8908f2a08b5e2621d9e2b713d2e95821ddd174bb
tchou8319-tech <tchou8319@gmail.com>
2026-07-28 16:01:19 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Bookstore Login");
         frame.setContentPane(new logIn().getMainPanel());
+        frame.setSize(400,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
